e3fec5af2cb67cf477f90679b06e6306aed61123
Vojtěch Stipper <vojtech.stipper@itixo.com>
2022-11-06 12:15:11 +0100

    init

--- src/app/car.ts
+++ src/app/car.ts
@@ -1,2 +1,15 @@
 export class Car {
+  name:string;
+  licencePlate:string;
+  fuelType:FuelType;
+
+  constructor(name: string, licencePlate: string, fuelType: FuelType) {
+    this.name = name;
+    this.licencePlate = licencePlate;
+    this.fuelType = fuelType;
+  }
+}
+export enum FuelType{
+  GAS = 0,
+  OIL = 1
 }
---
+++ src/app/carform/carform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carform',
+  templateUrl: './carform.component.html',
+  styleUrls: ['./carform.component.css']
+})
+export class CarformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
