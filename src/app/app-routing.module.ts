24d2cd215c68682faf99a2860feb9cbbffed0f20
Vojtěch Stipper <vojtech.stipper@itixo.com>
2022-12-03 09:32:41 +0100

    added charts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import {FuelformComponent} from "./fuelform/fuelform.component";
 import {UserRegistrationComponent} from "./user-registration/user-registration.component";
 import {UserLoginComponent} from "./user-login/user-login.component";
 import {FueltableComponent} from "./fueltable/fueltable.component";
+import {ChartpageComponent} from "./chartpage/chartpage.component";
 
 const routes: Routes = [
   {path: 'car-form', component: CarformComponent},
   {path: 'fuel-form', component: FuelformComponent},
   {path: 'fuel-table', component: FueltableComponent},
   {path: 'register', component: UserRegistrationComponent},
-  {path: 'login', component: UserLoginComponent}
+  {path: 'login', component: UserLoginComponent},
+  {path: 'chartpage', component: ChartpageComponent}
 ];
 
 @NgModule({
